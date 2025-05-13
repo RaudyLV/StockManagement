9caca55368f2d440c940ad27b7115f7127d527dd
Raudy <Raudy206@gmail.com>
2025-05-12 20:33:40 -0400

    Añadiendo middlewares personalizados

---
+++ src/Presentation/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Application.Exceptions;
+using FluentValidation;
+
+namespace Presentation.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try 
+            {
+                await _next(context);
+            }catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch(ex)
+            {
+                case ValidationException validationException:
+                code = HttpStatusCode.BadRequest;
+                var errors = validationException.Errors.Select(e => new {e.PropertyName, e.ErrorMessage});
+                result = JsonSerializer.Serialize(errors);
+                break;
+
+                case NotFoundException _:
+                code = HttpStatusCode.NotFound;
+                break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if(result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(
+                    new 
+                    {
+                        error = ex.Message,
+                        ex.StackTrace
+                    }
+                );
+            }
+
+            await context.Response.WriteAsync(result);
+        }
+
+    }
+}
